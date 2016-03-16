1a050924a031c0a99d20f357d6421c784d03d0fa
NIcnovGit <nicnovmail@gmail.com>
2016-03-17 00:28:01 +0300

    next3

--- hello.py
+++ hello.py
@@ -1,16 +1,7 @@
-from urlparse import parse_qs
-
-def application(environ, start_response):
-    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
-    body = []
-    for key, value in query.items():
-        for item in value:
-            body.append(key + "=" + item + "\r\n")
-
-    status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
-    ]
-
-    start_response(status, headers)
-    return body
+def app (environ, start_response):
+  status = '200 OK'
+  response_headers = [('Content-type','text/plain')]
+  start_response(status, response_headers)
+  resp = environ['QUERY_STRING'].split("&")
+  resp = [item+"\r\n" for item in resp]
+  return resp
